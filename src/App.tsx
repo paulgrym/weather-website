5132b1c55080099fc1204413c86b79f57b1d0764
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-07 10:28:25 +0100

    basic design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import "./App.css"
+
 function App() {
   return (
-    <div>
-      Temperature: <span className="temperature">10</span>
+    <div className="container">
+      <main className="main">
+        <h1 className="header">Pogoda na dziś</h1>
+      </main>
+      <footer className="footer">
+        <a
+          className="footer__link"
+          href='https://www.freepik.com/photos/sky'
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Sky photo created by lifeforstock
+        </a>
+      </footer>
     </div>
   );
 }
